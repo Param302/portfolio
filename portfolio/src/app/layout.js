e80422fd41a2ef42f6f7bbbcaf3f4c4d78e73372
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-18 03:21:52 +0530

    update: change location of favicon to app/

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { Roboto, Fira_Code } from "next/font/google";
 import "@/app/styles/globals.css";
 
@@ -23,18 +22,11 @@ const descriptionFont = Fira_Code({
 export const metadata = {
     title: "Parampreet Singh Portfolio",
     description: "A minimal portfolio of Parampreet Singh",
-    // icon: {
-    //     icon: "/favicon.ico",
-    // }
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            {/* <Head>
-                <link rel="shortcut icon" href="/favicon.ico" sizes="any" />
-                <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
-            </Head> */}
             <body
                 className={`${headingFont.className} ${descriptionFont.className} antialiased flex flex-col items-center py-12 md:py-16 lg:py-24`}
             >
