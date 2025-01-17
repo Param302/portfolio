fa895c6119f788b65832befd960d08d679885edd
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-18 03:00:47 +0530

    feat: add social handles

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,4 +1,5 @@
 import Email from '@/app/components/Email';
+import SocialHandles from './components/SocialHandles';
 import About from '@/app/components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
@@ -10,6 +11,7 @@ export default function Home() {
     return (
         <main className="w-4/5 lg:w-3/5 2xl:w-2/5 flex flex-col space-y-8 relative">
             <Email/>
+            <SocialHandles/>
             <About/>
             {/* <Experience/> */}
             <Projects/>
