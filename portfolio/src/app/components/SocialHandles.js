341b3b124efcdc84da827f81ad50e4ecce41c6dc
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-18 03:15:17 +0530

    feat: Add favicon

--- portfolio/src/app/components/SocialHandles.js
+++ portfolio/src/app/components/SocialHandles.js
@@ -1,6 +1,6 @@
 export default function SocialHandles() {
     return (
-        <section className="self-end fixed top-32 sm:top-24 right-6 md:right-auto flex flex-col space-y-8 sm:space-y-12 items-center z-50">
+        <section className="self-end fixed top-40 sm:top-24 right-6 md:right-auto flex flex-col space-y-8 sm:space-y-12 items-center z-50">
             <a href="https://github.com/Param302" target="_blank" rel="noopener noreferrer" title="github/@Param302"><img src="github.svg" className="w-9 h-9 sm:w-11 sm:h-11"/></a>
             <a href="https://www.linkedin.com/in/param302" target="_blank" rel="noopener noreferrer" title="linkedin/@Param302"><img src="linkedin.svg" className="w-8 h-8 sm:w-10 sm:h-10"/></a>
             <a href="https://x.com/Param3021" target="_blank" rel="noopener noreferrer" title="X/@Param3021"><img src="x.svg" className="w-8 h-8 sm:w-10 sm:h-10"/></a>
