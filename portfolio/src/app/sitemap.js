5e842305ebc1ce9f7471c92840dbc32457ca1fcb
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-18 18:45:28 +0530

    feat: Add sitemap

---
+++ portfolio/src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://param302.vercel.app/",
+            lastModified: new Date(),
+            changefreq: "weekly",
+            priority: 1
+        }
+    ]
+}
