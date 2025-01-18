2a5de3fa06563c9b3d5659517c057a2de58119e7
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-18 13:24:14 +0530

    update: add python talk in co-curricular

--- portfolio/src/app/components/CoCurricular.js
+++ portfolio/src/app/components/CoCurricular.js
@@ -3,10 +3,11 @@ export default function CoCurricular() {
         <section id="cocurricular" className="flex flex-col space-y-2">
             <h2 className="text-2xl md:text-3xl font-medium font-heading">Co-Curricular</h2>
             <ul className="list-disc list-inside flex flex-col space-y-2 font-medium text-base md:text-lg">
+                <li>Presented a <span className="font-semibold">Talk</span> on <a href="https://www.youtube.com/watch?v=08owIqXQebs" target="_blank" rel="noopener noreferrer" className="underline">Python 1 Liners</a>.</li>
                 <li>Handling a Coding Community of <span className="font-semibold">1 Lakh+</span> students.</li>
                 <li>Taught <a href="https://www.youtube.com/playlist?list=PLClULgPbRPsD-t0AYG8hR5iLIt2ZaNTkv" target="_blank" rel="noopener noreferrer" className="underline">Python</a> & <a href="https://www.youtube.com/playlist?list=PLClULgPbRPsA1twUfMlWkI4yJeqjsSi23" target="_blank" rel="noopener noreferrer" className="underline">Machine Learning</a> to <span className="font-semibold">100+</span> peers LIVE.</li>
-                <li>Made <span className="font-semibold">Websites</span> as Web Admin in college societies (<a href="https://synapse-society.github.io/Synapse/" target="_blank" rel="noopener noreferrer" className="underline">Synapse</a>, <a href="https://sahara-society.github.io/Sahara/" target="_blank" rel="noopener noreferrer" className="underline">Sahara</a>).</li>
                 <li>Created <a href="https://github.com/Param302/Discord-Verification-bot" target="_blank" rel="noopener noreferrer" className="underline">Verification</a> & <a href="https://github.com/Param302/settlement-design-challenege-bot" target="_blank" rel="noopener noreferrer" className="underline">Management</a> <span className="font-semibold">Discord Bot</span> for college events.</li>
+                <li>Made <span className="font-semibold">Websites</span> as Web Admin in college societies (<a href="https://synapse-society.github.io/Synapse/" target="_blank" rel="noopener noreferrer" className="underline">Synapse</a>, <a href="https://sahara-society.github.io/Sahara/" target="_blank" rel="noopener noreferrer" className="underline">Sahara</a>).</li>
             </ul>
         </section>
     )
