5542746368ce117ffd9069ef91e75ddaaf850021
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-18 17:42:08 +0530

    update: add alt text in social handles

--- portfolio/src/app/components/SocialHandles.js
+++ portfolio/src/app/components/SocialHandles.js
@@ -1,9 +1,9 @@
 export default function SocialHandles() {
     return (
         <section className="self-end fixed top-40 sm:top-24 right-6 md:right-auto flex flex-col space-y-8 sm:space-y-12 items-center z-50">
-            <a href="https://github.com/Param302" target="_blank" rel="noopener noreferrer" title="github/@Param302"><img src="github.svg" className="w-9 h-9 sm:w-11 sm:h-11"/></a>
-            <a href="https://www.linkedin.com/in/param302" target="_blank" rel="noopener noreferrer" title="linkedin/@Param302"><img src="linkedin.svg" className="w-8 h-8 sm:w-10 sm:h-10"/></a>
-            <a href="https://x.com/Param3021" target="_blank" rel="noopener noreferrer" title="X/@Param3021"><img src="x.svg" className="w-8 h-8 sm:w-10 sm:h-10"/></a>
+            <a href="https://github.com/Param302" target="_blank" rel="noopener noreferrer" title="github/@Param302"><img src="github.svg" alt="github/@Param302" className="w-9 h-9 sm:w-11 sm:h-11"/></a>
+            <a href="https://www.linkedin.com/in/param302" target="_blank" rel="noopener noreferrer" title="linkedin/@Param302"><img src="linkedin.svg" alt="linkedin/@Param302" className="w-8 h-8 sm:w-10 sm:h-10"/></a>
+            <a href="https://x.com/Param3021" target="_blank" rel="noopener noreferrer" title="X/@Param3021"><img src="x.svg" alt="X/@Param3021" className="w-8 h-8 sm:w-10 sm:h-10"/></a>
         </section>
     )
 }
